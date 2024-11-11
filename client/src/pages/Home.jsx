09ab5229e4546a50c9b12ee244692c6e9cb9e9e3
Akhileshramks <akhileshram710@gmail.com>
2024-11-11 12:17:27 +0530

    Added Vendor and Wishlist

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,27 +37,27 @@ const Home = () => {
       <div className="home-categories-container">
 
         <div className="home-category-card" onClick={()=>navigate('/category/Fashion')}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZQjXpWVVQhkT_A2n03XMo2KDV4yPSLBcoNA&usqp=CAU" alt="" />
+          <img src="https://img.freepik.com/premium-photo/stylish-fashion-model-trendy-suit-male-dress-with-sandy-beige-lifestyle-mens-portrait-fashion-male-background-lifestyle-portrait_171965-42448.jpg" alt="" />
           <h5>Fashion</h5>
         </div>
 
         <div className="home-category-card" onClick={()=>navigate('/category/Electronics')}>
-          <img src="https://5.imimg.com/data5/ANDROID/Default/2023/1/SE/QC/NG/63182719/product-jpeg-500x500.jpg" alt="" />
+          <img src="https://www.zdnet.com/a/img/resize/2b558241b3a7af8c476b3f6f5bfd26a012494ed1/2023/09/10/f87eda0e-6717-4b19-8829-170e2c098428/airpods-pro-2.jpg?auto=webp&fit=crop&height=900&width=1200" alt="" />
           <h5>Electronics</h5>
         </div>
 
         <div className="home-category-card" onClick={()=>navigate('/category/mobiles')}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jUW7v1WFJL9Ylax9a4vazyKXwG-ktSinI4Rd7qi7MkhMr79UlIyyrNkbiK0Cz5u6WYw&usqp=CAU" alt="" />
+          <img src="https://suprememobiles.in/cdn/shop/files/mobiles.jpg?crop=center&height=2048&v=1664280076&width=2048" alt="" />
           <h5>Mobiles</h5>
         </div>
 
         <div className="home-category-card" onClick={()=>navigate('/category/Groceries')}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXbpV_yQ_zCtZt_1kNebjvFqXvdDnLuuJPsQ&usqp=CAU" alt="" />
+        <img src="https://food-ubc.b-cdn.net/wp-content/uploads/2020/02/Save-Money-On-Groceries_UBC-Food-Services.jpg" alt="" />
           <h5>Groceries</h5>
         </div>
 
         <div className="home-category-card" onClick={()=>navigate('/category/Sports-Equipment')}>
-          <img src="https://a.storyblok.com/f/112937/568x464/82f66c3a21/all_the_english-_football_terms_you_need_to_know_blog-hero-low.jpg/m/620x0/filters:quality(70)/" alt="" />
+          <img src="https://img.freepik.com/premium-photo/soccer-ball-banner-template-football-tournament-competition-background-generative-ai_90099-7723.jpg" alt="" />
           <h5>Sports Equipments</h5>
         </div>
 
